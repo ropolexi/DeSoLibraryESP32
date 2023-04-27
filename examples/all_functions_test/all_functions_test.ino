2bc18278e76a92f992c0edb00b9077d6aabcf8da
ropolexi <83857790+ropolexi@users.noreply.github.com>
2023-04-27 22:46:15 +0530

    Update all_functions_test.ino

--- examples/all_functions_test/all_functions_test.ino
+++ examples/all_functions_test/all_functions_test.ino
@@ -91,9 +91,9 @@ void setup()
     delay(1000);
   }
 
-  deso.addNodePath("https://diamondapp.com",Baltimore_Root_caRootCert );
-  deso.addNodePath("https://node.deso.org", GTS_Root_caRootCert);
-  deso.addNodePath("https://desocialworld.com", ISRG_Root_caRootCert);
+  deso.addNodePath("https://desocialworld.com", GTS_Root_R1_caRootCert);
+  deso.addNodePath("https://diamondapp.com", Baltimore_Root_caRootCert);
+  deso.addNodePath("https://node.deso.org", GTS_Root_R1_caRootCert);
   deso.selectDefaultNode(0);
 }
 
