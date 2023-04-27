0b0cc4e7f24126fe3287b53f9e456ab709dfc7af
ropolexi <83857790+ropolexi@users.noreply.github.com>
2023-04-27 22:50:03 +0530

    Update lcd_16x2_dashboard.ino

--- examples/lcd_16x2_dashboard/lcd_16x2_dashboard.ino
+++ examples/lcd_16x2_dashboard/lcd_16x2_dashboard.ino
@@ -35,9 +35,9 @@ void setup()
     delay(1000);
     Serial.print(".");
   }
-  deso.addNodePath("https://desocialworld.com", ISRG_Root_caRootCert);
-  deso.addNodePath("https://diamondapp.com",Baltimore_Root_caRootCert );
-  deso.addNodePath("https://node.deso.org", GTS_Root_caRootCert);
+  deso.addNodePath("https://desocialworld.com", GTS_Root_R1_caRootCert);
+  deso.addNodePath("https://diamondapp.com", Baltimore_Root_caRootCert);
+  deso.addNodePath("https://node.deso.org", GTS_Root_R1_caRootCert);
 
   deso.selectDefaultNode(0);
   lcd.clear();
